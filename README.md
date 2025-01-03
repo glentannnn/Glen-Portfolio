effa79d7d1239ce980fd7eeb990113b10eb74c9a
Glen <glen.tanyonghao@gmail.com>
2025-01-04 00:15:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React-3D-Portfolio
+ Personal 3D Portfolio
---
+++ src/components/Loader.jsx
@@ -0,0 +1,33 @@
+import { Html, useProgress } from "@react-three/drei";
+// import { progress } from "framer-motion";
+
+const Loader = () => {
+  const { progress } = useProgress();
+
+  return (
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#f1f1f1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default Loader;
